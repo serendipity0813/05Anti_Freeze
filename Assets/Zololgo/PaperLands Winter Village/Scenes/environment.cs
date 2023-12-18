7a25687c282c229461e2f1c1f0e343c2bef276e3
heyjude0 <wordpressfirst1@gmail.com>
2023-12-18 18:30:29 +0900

    summary

---
+++ Assets/Zololgo/PaperLands Winter Village/Scenes/environment.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class environment : MonoBehaviour
+{
+    public GameObject rock;
+    public GameObject tree;
+    public GameObject coal;
+    public GameObject rock1;
+    public GameObject tree1;
+    public GameObject coal1;
+    public GameObject rock2;
+    public GameObject tree2;
+    public GameObject coal2;
+    public GameObject rock3;
+    public GameObject tree3;
+    public GameObject coal3;
+    public GameObject rock4;
+    public GameObject tree4;
+    public GameObject coal4;
+    public GameObject rock5;
+    public GameObject tree5;
+    public GameObject coal5;
+    public GameObject rock6;
+    public GameObject tree6;
+    public GameObject coal6;
+    public GameObject rock7;
+    public GameObject tree7;
+    public GameObject coal7;
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+
+    public void Environmentinstiate()
+    {
+        
+    }
+}
