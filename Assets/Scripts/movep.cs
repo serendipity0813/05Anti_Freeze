8a865dbafb59da74cdb6884941308b4160eeaaef
종 욱 <whddnr0317@naver.com>
2023-12-18 14:14:55 +0900

    [ADD] Monster, Ai Nav

---
+++ Assets/Scripts/movep.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class movep : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody charRigidbody;
+
+    void Start()
+    {
+        charRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 inputDir = new Vector3(hAxis, 0, vAxis).normalized;
+
+        charRigidbody.velocity = inputDir * moveSpeed;
+    }
+}
