bbcc3f3df61a5206def70167e3fe794b45bf11de
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-12-19 15:20:50 +0900

    House Collider

--- Assets/Scripts/Architect/BuildingInterect.cs
+++ Assets/Scripts/Architect/BuildingInterect.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingInterect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float range;
+    private RaycastHit hitInfo;
+    private LayerMask layerMask;
+    public GameObject interectText;
+    public GameObject buildingUI;
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckBuilding();
+
+        if(interectText.activeSelf == true && Input.GetKeyDown(KeyCode.E))
+        {
+            InterectUIOff();
+            BuildingUIOn();
+        }
     }
+
+
+    private void CheckBuilding()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, range, layerMask))
+        {
+            if (hitInfo.transform.tag == "Building")
+            {
+                InterectUIOn();
+                if (interectText.activeSelf == true && Input.GetKeyDown(KeyCode.E))
+                {
+                    InterectUIOff();
+                    BuildingUIOn();
+                }
+            }
+        }
+        else
+            InterectUIOff();
+    }
+
+    private void InterectUIOn()
+    {
+        interectText.SetActive(true);
+    }
+
+    private void InterectUIOff()
+    {
+        interectText.SetActive(false);
+    }
+    private void BuildingUIOn()
+    {
+        buildingUI.SetActive(true);
+    }
+
+
 }
