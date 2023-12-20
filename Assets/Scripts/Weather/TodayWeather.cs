d1aacd2b3a15e188c3fb089230b3487e09d8db9d
junyeong-0825 <scv9761@gmail.com>
2023-12-20 15:45:38 +0900

    [add] 날씨아이콘과 주기적으로 변하는 날씨 정보
    500초마다 날씨 정보인 기온과 기상상태가 바뀌도록 함(WeatherManager.cs)

---
+++ Assets/Scripts/Weather/TodayWeather.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeatherManager : MonoBehaviour
+{
+    public float WeatherStatus;
+    public float Temperature;
+    public Image WeatherIcon;
+    private void TodayWeather()
+    {
+        WeatherStatus = Random.Range(0, 3);
+        ///0¿Ã∏È ∏º¿Ω
+        ///1¿Ã∏È »Í∏≤
+        ///2¿Ã∏È ¥´≥ª∏≤
+    }
+    private void TodayTemperature()
+    {
+        Temperature = Random.Range(-10f, 10f);
+    }
+    void Start()
+    {
+        
+    }
+
+    IEnumerable ChangeWeather()
+    {
+        yield return new WaitForSeconds(500f);
+        TodayWeather();
+        TodayTemperature();
+    }
+
+}
