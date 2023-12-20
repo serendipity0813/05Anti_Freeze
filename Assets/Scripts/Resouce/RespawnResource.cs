dbece5c594f8b3e6cabdb7d3e5129ce81fe65588
종 욱 <whddnr0317@naver.com>
2023-12-20 16:25:43 +0900

    [Add] resource추가, 자원 리스폰

---
+++ Assets/Scripts/Resouce/RespawnResource.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RespawnResource : MonoBehaviour
+{
+    public static RespawnResource instance;
+    private float _spawntime;
+    private float _respawntime;
+    public GameObject[] ResourcePrefabs;
+
+    public int count = 0;
+    private GameObject spawnResource;
+    private int _respawnRate = 3;
+    private bool _respawnCheck;
+
+    private Queue<GameObject> _spawnQueue = new Queue<GameObject>();
+
+    private Vector3 _spawnPos;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Update()
+    {
+        _respawntime = _respawntime + Time.deltaTime;
+        _spawntime = _spawntime + Time.deltaTime;
+
+        spawn();
+    }
+
+
+    public void spawn()
+    {
+        if (_respawnRate - _spawntime < 0 && count < 5)
+        {
+            float randomX = Random.Range(-10f, 10f);
+            float randomY = 0f;
+            float randomz = Random.Range(-10f, 10f);
+            _spawnPos = new Vector3(randomX, randomY, randomz);
+
+            int selection = Random.Range(0, ResourcePrefabs.Length);
+
+            GameObject selectedPrefab = ResourcePrefabs[selection];
+            spawnResource = Instantiate(selectedPrefab, _spawnPos, Quaternion.identity);
+
+            _spawnQueue.Enqueue(spawnResource);
+            _spawntime = 0;
+            count++;
+            Debug.Log(count);
+        }
+    }
+}
